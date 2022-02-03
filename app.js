e83eac7529bad1c5810953384e30d3b357fbcdae
occasionalcode <occasionalprogrammer@gmail.com>
2022-02-04 02:19:45 +0800

    updated Js file

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const searchBtn = document.getElementById("search-btn");
-const mealList = document.getElementById("meal");
-const mealDetailsContent = document.querySelector(".meal-details-content");
+const searchButton = document.getElementById("search-btn");
+const foodList = document.getElementById("meal");
+const mealContent = document.querySelector(".meal-details-content");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
-// event listeners
-searchBtn.addEventListener("click", getMealList);
-mealList.addEventListener("click", getMealRecipe);
+searchButton.addEventListener("click", getfoodList);
+foodList.addEventListener("click", getMealRecipe);
 recipeCloseBtn.addEventListener("click", () => {
-  mealDetailsContent.parentElement.classList.remove("showRecipe");
+  mealContent.parentElement.classList.remove("showRecipe");
 });
 
-// get meal list that matches with the ingredients
-function getMealList() {
+function getfoodList() {
   let searchInputTxt = document.getElementById("search-input").value.trim();
   fetch(
     `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
@@ -33,47 +31,12 @@ function getMealList() {
                     </div>
                 `;
         });
-        mealList.classList.remove("notFound");
+        foodList.classList.remove("notFound");
       } else {
         html = "Sorry, we didn't find any meal!";
-        mealList.classList.add("notFound");
+        foodList.classList.add("notFound");
       }
 
-      mealList.innerHTML = html;
+      foodList.innerHTML = html;
     });
-}
-
-// get recipe of the meal
-function getMealRecipe(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("recipe-btn")) {
-    let mealItem = e.target.parentElement.parentElement;
-    fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
-    )
-      .then((response) => response.json())
-      .then((data) => mealRecipeModal(data.meals));
-  }
-}
-
-// create a modal
-function mealRecipeModal(meal) {
-  console.log(meal);
-  meal = meal[0];
-  let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-  mealDetailsContent.innerHTML = html;
-  mealDetailsContent.parentElement.classList.add("showRecipe");
 }
